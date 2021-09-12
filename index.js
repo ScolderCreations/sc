83e1d3d19be56cd28196e880ce610284ad2c1749
Scolder <69083943+ScolderCreations@users.noreply.github.com>
2021-09-11 17:09:13 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const Scratch = require("scratch3-api");
+
+
+async function main() {
+  let session = await Scratch.UserSession.create(window.prompt("Enter username:", ""), window.prompt("Enter password:", ""));
+}
+
+main();
+
+
+// localStorage.setItem("sessionid", sessionid);
